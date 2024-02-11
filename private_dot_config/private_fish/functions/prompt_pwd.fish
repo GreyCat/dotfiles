9d8c031bb9ea4a7231bf60a6472376d9b648ff85
Mikhail Yakshin <greycat.na.kor@gmail.com>
2024-02-11 19:01:46 +0000

    fish: update prompt_pwd to modern fish 3.7

--- private_dot_config/private_fish/functions/prompt_pwd.fish
+++ private_dot_config/private_fish/functions/prompt_pwd.fish
@@ -1,6 +1,24 @@
-function prompt_pwd --description "Print the current working directory, shortened to fit the prompt"
-    set -l options 'h/help'
-    argparse -n prompt_pwd --max-args=0 $options -- $argv
+# Based on sources from fish
+# https://github.com/fish-shell/fish-shell/blob/master/share/functions/prompt_pwd.fish
+#
+# fish Copyright © 2005-2009 Axel Liljencrantz, 2009-2024 fish-shell contributors. fish is released under the GNU General Public License, version 2.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+function prompt_pwd --description 'short CWD for the prompt'
+    set -l options h/help d/dir-length= D/full-length-dirs=
+    argparse -n prompt_pwd $options -- $argv
     or return
 
     if set -q _flag_help
@@ -8,23 +26,49 @@ function prompt_pwd --description "Print the current working directory, shortene
         return 0
     end
 
-    # This allows overriding fish_prompt_pwd_dir_length from the outside (global or universal) without leaking it
+    set -q argv[1]
+    or set argv $PWD
+
+    set -ql _flag_d
+    and set -l fish_prompt_pwd_dir_length $_flag_d
+
     set -q fish_prompt_pwd_dir_length
     or set -l fish_prompt_pwd_dir_length 1
 
-    # Replace $HOME with "~"
-    set realhome ~
-    set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+    set -l fulldirs 0
+    set -ql _flag_D
+    and set -l fish_prompt_pwd_full_dirs $_flag_D
 
-    # Replace path prefixes from `gc_pwd_longs` with `gc_pwd_shorts`
-    for i in (seq (count $gc_pwd_longs))
-        set tmp (string replace -r '^'"$gc_pwd_longs[$i]"'($|/)' "$gc_pwd_shorts[$i]"'$1' -- $tmp)
-    end
+    set -q fish_prompt_pwd_full_dirs
+    or set -l fish_prompt_pwd_full_dirs 1
 
-    if [ $fish_prompt_pwd_dir_length -eq 0 ]
-        echo $tmp
-    else
-        # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
-        string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp
+    for path in $argv
+        # Replace $HOME with "~"
+        set -l realhome (string escape --style=regex -- ~)
+        set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $path)
+
+        # GreyCat: replace path prefixes from `gc_pwd_longs` with `gc_pwd_shorts`
+        for i in (seq (count $gc_pwd_longs))
+            set tmp (string replace -r '^'"$gc_pwd_longs[$i]"'($|/)' "$gc_pwd_shorts[$i]"'$1' -- $tmp)
+        end
+
+        if test "$fish_prompt_pwd_dir_length" -eq 0
+            echo $tmp
+        else
+            # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+            # with full-length-dirs components left at full length.
+            set -l full
+            if test $fish_prompt_pwd_full_dirs -gt 0
+                set -l all (string split -m (math $fish_prompt_pwd_full_dirs - 1) -r / $tmp)
+                set tmp $all[1]
+                set full $all[2..]
+            else if test $fish_prompt_pwd_full_dirs -eq 0
+                # 0 means not even the last component is kept
+                string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*' '$1' $tmp
+                continue
+            end
+
+            string join / -- (string replace -ar -- '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp) $full
+        end
     end
 end
