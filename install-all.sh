dea86d8e9bea2ccab1895b26c0b11ac678d6b79e
Mikhail Yakshin <greycat.na.kor@gmail.com>
2024-11-27 18:59:39 +0000

    install-all.sh: enforce yes on fish installation for Ubuntu

--- install-all.sh
+++ install-all.sh
@@ -8,7 +8,7 @@ if [ "$ID" = ubuntu ]; then
     # https://launchpad.net/~fish-shell/+archive/ubuntu/release-3
     sudo apt-add-repository --yes ppa:fish-shell/release-3
     sudo apt-get update
-    sudo apt-get install fish
+    sudo apt-get install -y fish
 
     sudo chsh -s /usr/bin/fish "$USER"
 fi
